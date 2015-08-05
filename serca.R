d2eb273b1efd79846c9b96e5d0120b71ca7403a2
Altan Orhon <pliklime@imap.cc>
2015-08-05 06:33:28 +0300

    Bl

--- serca.R
+++ serca.R
@@ -1,62 +1,75 @@
-library(dplyr)
 library(tools)
+library(iterators)
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(foreach)
 
-faset_target_meta <- function(f, Target.name) {
-  t <- f$orig.table
-  st <- t %>% filter(Target==Target.name)
-  info <- list()
-  info$top3 <- st %>% top_n(3, n)
-  info$setsize <- length(st$Assoc)
-  info$total <- sum(st$n)
-  return (info)
+
+set_os_locales <- function() {
+  if(.Platform$OS.type != "unix") {
+    if(Sys.getlocale("LC_CTYPE") != "en_US.UTF-8") {
+      Sys.setlocale("LC_CTYPE", "Turkish_Turkey.1254")
+      Sys.setlocale("LC_COLLATE", "Turkish_Turkey.1254")
+    }
+  }
 }
 
-load_frequencies <- function(path) {
-  f <- tbl_df(read.delim(path, stringsAsFactors = FALSE, fileEncoding="UTF-8"))
-  f <- f %>% arrange(Target, desc(n), Assoc)
-  f <- f %>% group_by(Target, Assoc) %>%
+
+
+load_meta <- function(path) {
+ tbl_df(read.delim(path, stringsAsFactors = FALSE, fileEncoding = "UTF-8"))
+}
+
+
+get_labels <- function(meta) {
+  unique(select(fmeta, Label))
+}
+
+
+
+load_one_frequency_table <- function(path, label) {
+  tbl_df(read.delim(path, stringsAsFactors = FALSE, fileEncoding="UTF-8"))  %>%
+    arrange(Target, desc(n), Assoc) %>%
+    mutate(Label = label)
+}
+
+rank_frequencies <- function(frequencies) {
+  frequencies %>% group_by(Label, Target) %>%
     mutate(Rank = dense_rank(desc(n)), 
            RankMax=rank(n, ties.method = "max"),
-           Probability=round((1/(sum(n)/n)), 3))
-  return(f)
+           Probability=round((1/(sum(n)/n)), 3)
+    )
 }
 
-idio <- function(t) {
-  t %>% filter(n == 1)
+nsubjects <- function(meta, label) {
+  meta %>% filter(Label==label) %>% select(Subjects)
+}
+get_setsizes <- function(frequencies, meta) {
+  setsizes <- frequencies %>% filter(n > 1, Assoc != '?') %>% group_by(Label, Target) %>% summarise(SetSize=n()) %>% arrange(Target, Label)
+  idios <- frequencies %>% filter(n == 1) %>% group_by(Label, Target) %>% summarise(Idio=n()) %>% arrange(Target, Label)
+  setsizes <- setsizes %>% left_join(idios)
+
+  
+  setsizes <- setsizes %>% mutate(Total=SetSize+Idio)
+  
+  
+  frequencies %>% left_join(select(meta, Label, Subjects))
+  blanks <- setsizes %>% mutate(Blanks =  )
+  setsizes <- setsizes %>% left_join(blanks) %>% mutate(Blanks= replace(Blanks, is.na(Blanks), 0))
+  setsizes <- setsizes %>% arrange(Target, Label, SetSize, )
+  return(setsizes)
 }
 
-nonidio <- function(t) {
-  t %>% filter(n > 1)
+get_setsizes_stats <- function(setsizes) {
+  setsizes %>% group_by(Label) %>% summarise(N = n(),
+                                             Mean = round(mean(SetSize), 2),
+                                             SD = round(sd(SetSize), 2), 
+                                             Median = round(median(SetSize, 2)))
 }
 
-nonq <- function(t) {
-  t %>% filter(Assoc != '?')
+get_blanks <- function(frequencies) {
+  frequencies %>% filter(Assoc == '?') %>% group_by(Target, Label) %>% summarise(Blanks=sum(n))
 }
+set_os_locales()
 
-get_setsizes <- function(t) {
-  t %>% nonidio() %>% nonq() %>% group_by(Target) %>% summarise(setsize=n())
-}
-
-
-#Sys.setlocale("LC_CTYPE", "Turkish_Turkey.1254")
-#Sys.setlocale("LC_COLLATE", "Turkish_Turkey.1254")
-
-
-
-
-
-#targets <- transmute(fa, )
-#fa <- mutate(fa, NumTargets = length(unique(frequencies$Target)))
-#fa <- mutate(fa, AssocSetSize = get_asetsizes(frequencies))
-#fa <- mutate(fa, NumIdio = nrow(idio(frequencies)))
-
-                    
-# umi.fas <- umi$table %>% nonidio() %>% nonq() %>% group_by(Target) %>% summarise(set_size=n())
-# tum.fas <- semi_join(tek.fas, umi.fas, by='Target')
-
-#tum = list()
-#tum$setsizes <- semi_join(tek$setsizes, umi$setsizes, by='Target')
-
-#ss <-  onl$setsizes %>% merge(umi$setsizes, by = 'Target', suffixes = c('.onl', '.umi')) %>% merge(tum$setsizes)
-#names(ss)[names(ss) == 'setsize'] <- 'setsize.tek'
-#colnames(ss) <- c('Target', 'SS.onl', 'SS.umi', 'SS.tek')
